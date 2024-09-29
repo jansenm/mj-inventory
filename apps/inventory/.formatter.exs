dcfe23a8041a3abb2395941031c4b813fc37269b
Michael Jansen <mike@michael-jansen.biz>
2024-09-29 18:27:36 +0200

    Inform about the reimplementation

--- apps/inventory/.formatter.exs
+++
@@ -1,5 +0,0 @@
-# SPDX-FileCopyrightText: 2021 Michael Jansen <info@michael-jansen.biz>
-# SPDX-License-Identifier: CC0-1.0
-[
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
-]
