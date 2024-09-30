fbf5056550dfc171061ac33a3efb75605072a2f5
Michael Jansen <mike@michael-jansen.biz>
2024-09-30 19:18:30 +0200

    current state

---
+++ docker/jenkins/seed.groovy
@@ -0,0 +1,23 @@
+multibranchPipelineJob('build') {
+    displayName("Build the project")
+    description("Build the project")
+    branchSources {
+        git {
+            id('project') // IMPORTANT: use a constant and unique identifier
+            remote('/srv/git/project')
+            includes('*')
+            excludes('archive/*')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
+
+    triggers {
+        periodicFolderTrigger {
+            interval("10m")
+        }
+    }
+}
